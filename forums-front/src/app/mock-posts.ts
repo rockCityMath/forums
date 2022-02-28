43c64ba03c805208b5d9e50b4a17cf1714427181
Natalio Castaneda <natalioc@icloud.com>
2022-02-28 08:25:25 -0600

    edit: front page posts

--- forums-front/src/app/mock-posts.ts
+++ forums-front/src/app/mock-posts.ts
@@ -7,4 +7,10 @@ export const POSTS: Post[] = [
   { id: 14, title: 'Forum Post About Something4', content: 'Lorem ipsum dolor sit amet4, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 40},
   { id: 15, title: 'Forum Post About Something5', content: 'Lorem ipsum dolor sit amet5, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 50},
   { id: 16, title: 'Forum Post About Something6', content: 'Lorem ipsum dolor sit amet6, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 60},
+  { id: 17, title: 'Forum Post About Something1', content: 'Lorem ipsum dolor sit amet1, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 10},
+  { id: 18, title: 'Forum Post About Something2', content: 'Lorem ipsum dolor sit amet2, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 20},
+  { id: 19, title: 'Forum Post About Something3', content: 'Lorem ipsum dolor sit amet3, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 30},
+  { id: 20, title: 'Forum Post About Something4', content: 'Lorem ipsum dolor sit amet4, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 40},
+  { id: 21, title: 'Forum Post About Something5', content: 'Lorem ipsum dolor sit amet5, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 50},
+  { id: 22, title: 'Forum Post About Something6', content: 'Lorem ipsum dolor sit amet6, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', likes: 60},
 ]
