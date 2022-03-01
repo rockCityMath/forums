61e4db7cb65b85aa351b6ea5ef6af5759048fa74
Natalio Castaneda <natalioc@icloud.com>
2022-02-28 18:46:38 -0600

    finished login/register, show compose on logged in

--- forums-front/src/app/compose/compose.component.ts
+++ forums-front/src/app/compose/compose.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-compose',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compose.component.scss']
 })
 export class ComposeComponent implements OnInit {
+  form!: FormGroup;
+  public loginInvalid?: boolean;
+  private formSubmitAttempt?: boolean;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      title: [''],
+      content: [''],
+      tags: ['']
+    });
   }
 
+  async onSubmit() {
+    this.loginInvalid = false;
+    this.formSubmitAttempt = false;
+    if (this.form?.valid) {
+      try {
+        await this.authService.login(this.form.value);
+      } catch (err) {
+        this.loginInvalid = true;
+      }
+    } else {
+      this.formSubmitAttempt = true;
+    }
+  }
 }
