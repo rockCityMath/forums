251d0800c54e9967e43a0a8a8087e0ec54ccf1e2
BuildTools <unconfigured@null.spigotmc.org>
2022-03-09 18:57:53 -0600

    Change structure of homepage

--- forums-back/db/index.js
+++ forums-back/db/index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://zachc:mongopass145-@cluster0.bagyi.mongodb.net/ForumApp?retryWrites=true&w=majority', { useNewUrlParser: true })
+    .connect('mongodb+srv://zachc:mongopass123@cluster0.bagyi.mongodb.net/ForumApp?retryWrites=true&w=majority', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
