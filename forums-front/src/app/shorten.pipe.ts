047e684f105c5f83c4fc1f6772dac5d2da6705bf
Natalio Castaneda <natalioc@icloud.com>
2022-03-18 03:19:18 -0500

    homepage style fixes

--- forums-front/src/app/shorten.pipe.ts
+++ forums-front/src/app/shorten.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core'
 })
 export class ShortenPipe implements PipeTransform {
   transform(val:string , length:number):string {
-    return val.length > length ? `${val.substring(0, length)} ...` : val
+    return val.length > length ? `${val.substring(0, length)}...` : val
   }
 }
