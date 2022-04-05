c33ca0ca4da018e4dac86cabee075492d7a89989
Natalio Castaneda <natalioc@icloud.com>
2022-04-05 08:52:14 -0500

    began adding styling to recent posts; added create post button

--- forums-front/src/app/home/home.component.ts
+++ forums-front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service'
+import { ServerService } from '../shared/services/server-interface.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService, private server: ServerService) { }
 
   ngOnInit(): void {
+    this.authService.isLoggedIn.subscribe(data => {
+      if(data) {
+        const nameObservable = this.server.getUsernameFromID()
+        nameObservable.subscribe((data ) => {
+          console.log(data)
+          data = Object.values(data)
+        })
+      }
+    })
   }
 
 }
