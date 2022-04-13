abf9b8ab13e94992b8d2a19e0998d07198945520
Natalio Castaneda <natalioc@icloud.com>
2022-04-13 00:19:10 -0500

    Register now automatically logs in user; Fixed footer issue

--- forums-front/src/app/register/register.component.ts
+++ forums-front/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ServerService } from '../shared/services/server-interface.service'
+import { AuthService } from '../shared/services/auth.service';
+import { waitForAsync } from '@angular/core/testing';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +12,23 @@ import { ServerService } from '../shared/services/server-interface.service'
 })
 
 export class RegisterComponent implements OnInit {
-  
+
   /*
   constructor() { }
 
   ngOnInit(): void {
   }
   */
-  
+
   form!: FormGroup;
+  public loginInvalid?: boolean;
+  private formSubmitAttempt?: boolean;
 
   constructor(
     private fb: FormBuilder,
     private server: ServerService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
@@ -49,10 +54,28 @@ export class RegisterComponent implements OnInit {
       //SHOW SUCCESS HERE
 
       console.log(data)
-      
-      alert("Successfully registered acount. Please sign in below!")
-      this.router.navigate(['/login']);
+
+      this.ifReg();
+
+      //alert("Successfully registered acount. Please sign in below!")
+      //this.router.navigate(['/login']);
     })
+
   }
-  
+
+  async ifReg() {
+    this.loginInvalid = false;
+    this.formSubmitAttempt = false;
+    if (this.form?.valid) {
+      try {
+       await this.authService.login(this.form.value);
+       return true;
+      } catch (err) {
+        this.loginInvalid = true;
+      }
+    } else {
+      this.formSubmitAttempt = true;
+    }
+  }
+
 }
