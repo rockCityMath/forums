3990e7b686f1254668ec7078e23e081b4345e40b
zsc0012 <zacharycarpenter@my.unt.edu>
2022-04-26 10:09:29 -0500

    user comment/stats

--- forums-front/src/app/user/user.component.ts
+++ forums-front/src/app/user/user.component.ts
@@ -15,7 +15,7 @@ export class UserComponent implements OnInit {
   posts: any[] = []
   user: any;
   userComments: any[] = []
-  userStats: any[] = []
+  userStats: any = []
   post: any;
   posts2: any;
   userOwnsPost: any = true
