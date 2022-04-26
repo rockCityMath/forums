cbf2f9356a0472f782a0344a9b6b0b6f7931cdd2
zsc0012 <zacharycarpenter@my.unt.edu>
2022-04-26 17:21:45 -0500

    Edit bio on user and admin page

--- forums-front/src/app/edit-bio/edit-bio.component.ts
+++ forums-front/src/app/edit-bio/edit-bio.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../shared/services/auth.service';
 import { ServerService } from '../shared/services/server-interface.service'
 import { Router } from '@angular/router'
 import { ActivatedRoute } from '@angular/router'
+import { Input } from '@angular/core'
 
 @Component({
   selector: 'app-edit-bio',
@@ -11,22 +12,53 @@ import { ActivatedRoute } from '@angular/router'
   styleUrls: ['./edit-bio.component.scss']
 })
 export class EditBioComponent implements OnInit {
+
+  @Input() userID: string = '';
+
   editBioForm!: FormGroup;
   public loginInvalid?: boolean;
   private formSubmitAttempt?: boolean;
-  userID: any = ''
   username: any = 'null'
   isAdmin: Boolean = false
   public user: any
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private server: ServerService, private router: Router, private route: ActivatedRoute ) {    route.params.subscribe(
-      (params) => {
-        this.userID = params['id']
-      });
-    this.getUserDetails();
+  constructor(private fb: FormBuilder, private authService: AuthService, private server: ServerService, private router: Router, private route: ActivatedRoute ) {    
+    // route.params.subscribe(
+    //   // (params) => {
+    //   //   this.userID = params['id']
+    //   // });
+
+    
+  }
+
+
+  ngOnChanges(): void {
+    // this.getUserDetails();
+
+    this.editBioForm = this.fb.group({
+      editBioContent: [''],
+    });
+
+    // this.authService.isLoggedIn.subscribe(data => {
+    //   if(data) {
+    //     const nameObservable = this.server.getUsernameFromID()
+    //     nameObservable.subscribe((data ) => {
+    //       console.log(data)
+    //       data = Object.values(data)
+    //       this.username = data[0]
+    //       this.isAdmin = data[1]
+    //     })
+    //     const idObservable = this.server.getUserID()
+    //     idObservable.subscribe((data: any) => {
+    //       this.userID = data.userID
+    //     })
+    //   }
+    // })
   }
 
   ngOnInit(): void {
+
+
     this.editBioForm = this.fb.group({
       editBioContent: [''],
     });
@@ -55,16 +87,20 @@ export class EditBioComponent implements OnInit {
       return;
     }
 
+    console.log("VALUE")
+    console.log(this.editBioForm.get('editBioContent')?.value)
+
     const request = this.server.updateUser(
       {biography: this.editBioForm.get('editBioContent')?.value},
       this.userID
       );
 
-    request.subscribe(() => {
-      //SHOW SUCCESS HERE
-
+    request.subscribe((data) => {
+      console.log("DATA")
+      console.log(data)
       alert("Successfully updated biography!")
-      this.router.navigate(['/user/' + this.userID]);
+      //this.router.navigate(['/user/' + this.userID]);
+      window.location.reload()
     })
   }
 
@@ -74,7 +110,7 @@ export class EditBioComponent implements OnInit {
       data = Object.values(data)
       this.user = data[1]
       this.editBioForm = this.fb.group({
-        biography: [this.user.editBioContent]
+        biography: ['']
       });
     })
   }
